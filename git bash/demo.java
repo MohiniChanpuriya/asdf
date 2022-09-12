65d7d0a7e439de8e1f70b52f38ffce66f3bab23f
MohiniChanpuriya <chanpuriyamohini51@gmail.com>
2022-09-12 17:43:31 +0530

    Initial commit

---
+++ git bash/demo.java
@@ -0,0 +1,11 @@
+class Test{
+    {
+        public static void main (String [] args)
+        {
+           // int String = 888;
+           // System.out.println(String);
+           int Runnable = 999;
+           Sytstem.out.println(Runnable);
+        }
+    }
+}
